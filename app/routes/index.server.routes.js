6813b1a4773dacdd848588e92e3f3efdc869e387
Baktash Sana <51730261+baktash-11@users.noreply.github.com>
2021-02-05 03:08:19 -0500

    to views and css  added to them

--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -7,7 +7,7 @@ module.exports = function (app) {
     //by showing the login screen
     app.get('/', function (req, res) {
         //display login page
-        res.render('index', { loginMessage: 'Please login' });
+        res.render('index', { loginMessage: 'Please login using your my.centennialcollege Eamil:' });
 
     });
     //the form uses a post request to the same path ('/')
